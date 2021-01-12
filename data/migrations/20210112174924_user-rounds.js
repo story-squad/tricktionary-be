68db6eab67baf59f517258e18d8a7871d3a84c34
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-12 18:28:58 -0500

    port knex

---
+++ data/migrations/20210112174924_user-rounds.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("User-Rounds", (tbl) => {
+    tbl.increments("id");
+    tbl.string("user_id", 255).notNullable();
+    tbl
+      .integer("round_id")
+      .unsigned()
+      .notNullable()
+      .references("Rounds.id")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("User-Rounds");
+};
---
+++ knexfile.js
@@ -0,0 +1,22 @@
+var dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
+  },
+};
