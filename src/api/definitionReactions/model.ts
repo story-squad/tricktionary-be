4d7ef31411e009d5289fd687f862dabbb8ac00f2
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-13 21:53:58 -0500

    definition reactions

--- src/api/definitionReactions/model.ts
+++ src/api/definitionReactions/model.ts
@@ -1,6 +1,15 @@
 import db from "../../dbConfig";
+export default {
+  add,
+  getByReaction,
+  getByRound,
+  getByDefinition,
+  getByUser,
+  getFinished,
+  getUnfinished
+}
 
-export function add(
+function add(
   userId: string,
   roundID: number,
   reactionID: number,
@@ -18,26 +27,26 @@ export function add(
     .returning("id");
 }
 
-export function getByRound(roundID: number) {
+function getByRound(roundID: number) {
   return db("Definition-Reactions").where({ round_id: roundID });
 }
 
-export function getByUser(userID: string) {
+function getByUser(userID: string) {
   return db("Definition-Reactions").where({ user_id: userID });
 }
 
-export function getByReaction(reactionID: number) {
+function getByReaction(reactionID: number) {
   return db("Definition-Reactions").where({ reaction_id: reactionID });
 }
 
-export function getByDefinition(definitionID: number) {
+function getByDefinition(definitionID: number) {
   return db("Definition-Reactions").where({ definition_id: definitionID });
 }
 
-export function getFinished() {
+function getFinished() {
   return db("Definition-Reactions").where({ game_finished: true });
 }
 
-export function getUnfinished() {
+function getUnfinished() {
   return db("Definition-Reactions").where({ game_finished: false });
 }
