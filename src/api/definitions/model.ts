30474bfbbc8b5b01e3b46c6f50cd9c45cc932b03
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-13 20:30:28 -0500

    fix model type

--- src/api/definitions/model.ts
+++ src/api/definitions/model.ts
@@ -1,11 +1,11 @@
 import db from "../../dbConfig";
 import { validateDefinition } from "./utils";
 
-export function add(userID: string, definitionID: number, roundID: number) {
+export function add(userID: string, definition: string | null, roundID: number) {
   // validate object.property types
   const newDefinition = validateDefinition({
     user_id: userID,
-    definition_id: definitionID,
+    definition,
     round_id: roundID
   });
   return newDefinition.ok
