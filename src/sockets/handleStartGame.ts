4d6d3649466ff8e400061a95569ebfa457e2568a
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-15 18:59:49 -0500

    start a new game

--- src/sockets/handleStartGame.ts
+++ src/sockets/handleStartGame.ts
@@ -1,37 +1,67 @@
-import axios from "axios";
+import { localAxios } from "./common";
 
-async function handleStartGame(io: any, socket: any, lobbyCode: any, lobbies: any) {
-  const phase:string = "WRTITING";
+async function handleStartGame(
+  io: any,
+  socket: any,
+  lobbyCode: any,
+  lobbies: any
+) {
+  const phase: string = "WRTITING";
   // get random word
-  const { data } = await axios.get("/api/words");
-  const word = {
-    id: data?.id,
-    word: data?.word,
-    definition: data?.definition || null
+  let word: any;
+  let output: any;
+  try {
+    output = await localAxios.get("/api/words");
+    word = output?.data?.word;
+    // console.log(word)
+  } catch (err) {
+    console.log(err);
   }
+
   // guard
-  if (!word || !word.id) return false
+  if (!word?.id) return false;
   // start a new round
-  const newRound:any = await axios.post("api/rounds/start",  { lobby: lobbies[lobbyCode], wordId: word.id });
-  const roundId = newRound?.data?.roundId;
-  if (!roundId) return false
+  let newRound: any;
+  let roundId: any;
+  try {
+    console.log('starting a new round...')
+    newRound = await localAxios.post("/api/round/start", {
+      lobby: lobbies[lobbyCode],
+      wordId: word.id
+    });
+    roundId = newRound.data?.roundId;
+  } catch (err) {
+    console.log('error!')
+    // console.log(err);
+  }
+  console.log("ROUND ID:", roundId)
+  // if (!roundId) return false;
   // set phasers to "WRITING" and update the game state
   lobbies[lobbyCode] = {
     ...lobbies[lobbyCode],
-          phase,
-          word: word.word,
-          definition: word.definition,
-          roundId
-  }
+    phase,
+    word: word.word,
+    definition: word.definition,
+    roundId
+  };
   // REST-ful update
-  const result:any = await axios.post("api/user-rounds/add-players", {players: lobbyCode[lobbyCode].players, roundId});
-  if (result.status === 201) {
-    console.log('game update...')
+  let result: any;
+  try {
+    result = await localAxios.post("/api/user-rounds/add-players", {
+      players: lobbies[lobbyCode].players,
+      roundId
+    });
+  } catch (err) {
+    console.log('error: handleStartGame:55')
+    // console.log(err);
+  }
+  if (result?.status === 201) {
+    // console.log("game update...");
     // pub-sub update
     io.to(lobbyCode).emit("game update", lobbies[lobbyCode]);
   } else {
-    console.log("error updating game")
-    console.log(result)
+    console.log("error updating game");
+    // console.log(result);
   }
 }
 
