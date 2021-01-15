32e8ecf9d20c8921b1fd8f2453b316b04140d2b4
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-14 22:53:04 -0500

    handleLobbyJoin

---
+++ src/sockets/handleLobbyJoin.ts
@@ -0,0 +1,28 @@
+import { LC_LENGTH } from "./common";
+
+function handleLobbyJoin(
+  io: any,
+  socket: any,
+  username: string,
+  lobbyCode: any,
+  lobbies: any
+) {
+  if (lobbyCode.length !== LC_LENGTH) {
+    return;
+  }
+
+  socket.join(lobbyCode);
+
+  if (lobbies[lobbyCode] && lobbies[lobbyCode].players) {
+    lobbyCode[lobbyCode] = {
+      ...lobbies[lobbyCode],
+      player: [
+        ...lobbies[lobbyCode].players,
+        { id: socket.id, username, definition: "", points: 0 }
+      ]
+    };
+  }
+  io.to(lobbyCode).emit("game update", lobbies[lobbyCode]);
+  console.log(lobbies[lobbyCode]);
+}
+export default handleLobbyJoin
