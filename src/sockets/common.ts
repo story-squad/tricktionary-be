b31f8d60222cdf34fc92a97ad538272c18ddeb09
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-15 18:56:10 -0500

    fix baseURL on localAxios

--- src/sockets/common.ts
+++ src/sockets/common.ts
@@ -1,1 +1,10 @@
-export const LC_LENGTH: number = 4; // number of characters in lobbyCode
+// import * as dotenv from "dotenv";
+import axios from "axios";
+import * as dotenv from "dotenv";
+dotenv.config();
+const localAxios = axios.create({
+  baseURL: `${process.env.BASE_URL}:${process.env.PORT}`
+});
+localAxios.defaults.timeout = 10000;
+const LC_LENGTH: number = 4; // number of characters in lobbyCode
+export { LC_LENGTH, localAxios };
