ee2440651a69ea995d6026981cfbacf7c5be8c70
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-15 18:40:18 -0500

    fix resolve routes

--- src/api/rounds/routes.ts
+++ src/api/rounds/routes.ts
@@ -5,17 +5,15 @@ const router = Router();
 
 router.post("/start", async (req, res) => {
   const { lobby, wordId } = req.body;
-  if (lobby && wordId) {
-    const [roundId] = await Rounds.add(lobby, wordId);
-    res.send(201).json({ roundId }); // return ID of new round
-  }
+  const result = await Rounds.add(lobby, wordId);
+  res.status(201).json({ roundId: Array.from(result).pop() });
 });
 
 router.post("/finish", async (req, res) => {
-  const {roundId} = req.body;
-  const result = await Rounds.roundFinished(Number(roundId))
-  res.send(200).json({result})
-  console.log(result)
+  const { roundId } = req.body;
+  const result = await Rounds.roundFinished(Number(roundId));
+  res.send(200).json({ result });
+  console.log(result);
 });
 
 export default router;
