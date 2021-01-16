29919a807da6f2a62f3f140ed94cceaea5fca07e
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-16 01:13:00 -0500

    add definition

--- src/api/definitions/routes.ts
+++ src/api/definitions/routes.ts
@@ -4,12 +4,20 @@ import Definitions from "./model";
 const router = Router();
 
 router.post("/new", async (req, res) => {
-  const { playerID, definition, roundId } = req.body
-  const [ defId, ...msg ]  = await Definitions.add(playerID, definition, roundId);
-  if (defId > -1) {
+  const { playerId, definition, roundId } = req.body
+  // console.log(`new DEFINITION: ${definition}`)
+  // console.log(`playerID: ${playerId}`)
+  let defId:any;
+  try {
+    defId = await Definitions.add(playerId, definition, roundId);
+  } catch (err) {
+    console.log('error! definitions router')
+  }
+  // const [ defId, ...msg ]  =
+  if (defId) {
     res.send(201).json({definitionId: defId})
   } else {
-    res.send(400).json({ error: msg })
+    res.send(400).json({ error: "failed to get definition Id" })
   }
 });
 
