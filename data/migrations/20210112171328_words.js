bf8613e6a93fb5763ed07794a1094ea460d99b59
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-17 23:41:36 -0500

    fix: character limited word rejection

--- data/migrations/20210112171328_words.js
+++ data/migrations/20210112171328_words.js
@@ -1,8 +1,8 @@
 exports.up = function (knex) {
   return knex.schema.createTable("Words", (tbl) => {
     tbl.increments("id");
-    tbl.string("word").notNullable().unique();
-    tbl.string("definition").notNullable();
+    tbl.string("word", 1000).notNullable().unique();
+    tbl.string("definition", 1000).notNullable();
     tbl.boolean("moderated").defaultTo(false);
     tbl.boolean("approved").defaultTo(false);
   });
