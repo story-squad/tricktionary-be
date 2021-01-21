f0194402d63630cc17bf6b2b28a93fa38cff214e
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-21 17:05:36 -0500

    add error handler

---
+++ src/sockets/handleErrorMessage.ts
@@ -0,0 +1,17 @@
+import { privateMessage } from "./common";
+
+/**
+ * emit "error" message to player at socket.id
+ * @param io any (socketio)
+ * @param socket any (socketio)
+ * @param error string 
+ */
+async function handleErrorMessage(io: any, socket: any, error: string) {
+  try {
+    privateMessage(io, socket, "error", error);
+  } catch (err) {
+    console.log({ error: err });
+  }
+}
+
+export default handleErrorMessage;
