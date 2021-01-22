1e95f4bc6c88eb46f6800d5c98b20a4dc3f5f3f0
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-22 14:05:48 -0500

    use privateMessage

--- src/sockets/handleFortune.ts
+++ src/sockets/handleFortune.ts
@@ -1,14 +1,12 @@
-import { fortune } from "./common";
-import handleErrorMessage from "./handleErrorMessage";
+import { fortune, privateMessage } from "./common";
 
 async function handleFortune(io: any, socket: any) {
   try {
-    const pid = socket.id;
     const result = await fortune();
-    io.to(pid).emit("fortune", result.fortune); // private message fortune to player
+    privateMessage(io, socket, "fortune", result.fortune)
   } catch (err) {
     console.log({ error: err });
-    handleErrorMessage(io, socket, err);
+    privateMessage(io, socket, "error", err);
   }
 }
 
