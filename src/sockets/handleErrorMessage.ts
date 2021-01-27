3b6ab8ef2453841eb5cee0606755adec511ac82a
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-27 01:11:19 -0500

    prettier

--- src/sockets/handleErrorMessage.ts
+++ src/sockets/handleErrorMessage.ts
@@ -4,7 +4,7 @@ import { privateMessage } from "./common";
  * emit "error" message to player at socket.id
  * @param io any (socketio)
  * @param socket any (socketio)
- * @param error string 
+ * @param error string
  */
 async function handleErrorMessage(io: any, socket: any, error: string) {
   try {
