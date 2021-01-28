6e32a1104855e76491143f115d2514ef4fda42e8
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-27 23:17:42 -0500

    fix: handleSetPhase

--- src/sockets/handleSetPhase.ts
+++ src/sockets/handleSetPhase.ts
@@ -2,9 +2,9 @@ import { privateMessage, playerIsHost } from "./common";
 
 
 /**
- * 
+ *
  * Allows the host to change game state. *experimental feature
- * 
+ *
  * @param io (socketio)
  * @param socket (socketio)
  * @param lobbyCode key-string
@@ -12,11 +12,15 @@ import { privateMessage, playerIsHost } from "./common";
  * @param phase gamestate-string
  */
 async function handleSetPhase(io:any, socket: any, lobbyCode: any, lobbies: any, phase: string) {
-  if (playerIsHost(socket, lobbyCode, lobbies).ok) {
+  const checkIfHost = playerIsHost(socket, lobbyCode, lobbies);
+  if (checkIfHost.ok) {
     console.log(`host is setting phase : ${phase}`);
     lobbies[lobbyCode].phase = phase;
     privateMessage(io, socket, "info", `ok, set phase: ${phase}`)
     io.to(lobbyCode).emit("game update", lobbies[lobbyCode]);
+  } else {
+    console.log(`NOT HOST: ${socket.id}`);
+    privateMessage(io, socket, "error", "unauthorized call, punk!")
   }
 }
 
