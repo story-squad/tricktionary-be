8be0325e6c9e1381290df8cd684ebe98be3a2761
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-31 18:35:18 -0500

    changed port to 3000

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,8 @@ import { AddressInfo } from "net";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-
-const PORT: any = process.env.PORT;
-const portNumber: number = Number(PORT || 8081);
-
-const server = socketApp.listen(portNumber, "0.0.0.0", () => {
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+const server = socketApp.listen(PORT, "0.0.0.0", () => {
   const { port, address } = server.address() as AddressInfo;
-  console.log(`Server listening @ http://${address}:${port}`); // for devs convenience
+  console.log(`Server listening @ http://${address}${port ? `:${port}` : ""}`); // for devs convenience
 });
