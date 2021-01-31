19fe1382a07c6429c4f438411b216c43b7f2bf58
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-31 17:39:19 -0500

    default port 8081

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const PORT: any = process.env.PORT;
-const portNumber: number = Number(PORT || 8080);
+const portNumber: number = Number(PORT || 8081);
 
 const server = socketApp.listen(portNumber, "0.0.0.0", () => {
   const { port, address } = server.address() as AddressInfo;
--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,5 @@
 import fs from 'fs';
 
-const log = function(entry:string) {
+export const log = function(entry:string) {
   fs.appendFileSync('/tmp/trixie.log', `${new Date().toISOString()} - ${entry}\n`);
 };
-
-
-export { log }
