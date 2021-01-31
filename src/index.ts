2bb52a1e2aa2def4bac67dec365ffcde55436c91
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-31 18:51:05 -0500

    set API_PORT

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { AddressInfo } from "net";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
-const server = socketApp.listen(PORT, "0.0.0.0", () => {
+const apiPort = process.env.API_PORT ? Number(process.env.API_PORT) : 8080;
+const server = socketApp.listen(apiPort, "0.0.0.0", () => {
   const { port, address } = server.address() as AddressInfo;
   console.log(`Server listening @ http://${address}${port ? `:${port}` : ""}`); // for devs convenience
 });
