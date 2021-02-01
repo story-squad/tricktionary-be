3991c32a680a1e8d84fe0e45ac678eb3342d880d
Trevor Martin <trevor.j.martin@gmail.com>
2021-01-31 19:04:45 -0500

    verified nginx expects port 8080

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import { AddressInfo } from "net";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-const apiPort = process.env.API_PORT ? Number(process.env.API_PORT) : 8080;
-const server = socketApp.listen(apiPort, "0.0.0.0", () => {
+const server = socketApp.listen(8080, "127.0.0.1", () => {
   const { port, address } = server.address() as AddressInfo;
   console.log(`Server listening @ http://${address}${port ? `:${port}` : ""}`); // for devs convenience
 });
