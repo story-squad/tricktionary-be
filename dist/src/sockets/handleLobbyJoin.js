1fdaee933f8edcdb6d2d0f069935e3eaae61ab08
Trevor Martin <trevor.j.martin@gmail.com>
2021-02-10 16:26:11 -0500

    game update on join

--- dist/src/sockets/handleLobbyJoin.js
+++ dist/src/sockets/handleLobbyJoin.js
@@ -37,7 +37,7 @@ function handleLobbyJoin(io, socket, username, lobbyCode, lobbies) {
     io.to(playerId).emit("welcome", playerId); // private message new player with id  
     // ask others to add this player
     io.to(lobbyCode).emit("add player", { id: socket.id, username, definition: "", points: 0 });
-    // io.to(lobbyCode).emit("game update", lobbies[lobbyCode]); // ask room to update
+    io.to(lobbyCode).emit("game update", lobbies[lobbyCode]); // ask room to update
     // right now, sending just the player list knocks everyone out of the room. sending the "game update" works.
     // io.to(lobbyCode).emit("player list", lobbies[lobbyCode].players); // send player list
     console.log(lobbies[lobbyCode]);
