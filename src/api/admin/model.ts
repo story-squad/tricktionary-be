5b5ee3918065f8e391ff71a96b246db050e4201f
Lucas Greenwell <lucasgreenwell@Lucass-MacBook-Pro.local>
2021-02-13 10:53:34 -0500

    adds model for choice-tracking

--- src/api/admin/model.ts
+++ src/api/admin/model.ts
@@ -1,6 +1,7 @@
 import db from "../../dbConfig";
+import {validateHostChoice} from './utils';
 
-export default { getRound };
+export default { getRound, addHostChoice };
 
 /**
  * Round round get around, I get around, yeah
@@ -26,4 +27,14 @@ async function getRound(roundId: number) {
   // I'm a real cool head (get around round round I get around)
   // I'm makin' real good bread (get around round round I get around)
   return { word, round, players, definitions, definitionReactions, votes }
+}
+
+async function addHostChoice(word_id_one: number, word_id_two: number, round_id: number, times_shuffled: number){
+  // validate object.property types
+  const newHostChoice = validateHostChoice({
+    word_id_one, word_id_two, round_id, times_shuffled
+  });
+  return newHostChoice.ok
+    ? db("host-choices").insert(newHostChoice.value).returning("id")
+    : [ -1, newHostChoice.message ];
 }
