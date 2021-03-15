855fef045deaa661842a93d3479fd0a3082316de
trevorjmartin <trevor.j.martin@gmail.com>
2021-03-14 20:34:29 -0400

    write to disk, optional

--- src/logger.ts
+++ src/logger.ts
@@ -1,15 +1,35 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
+const startTime = Date.now();
+function getLogFilePaths() {
+  const filename = process.env.LOGFILE
+    ? `${startTime}-${process.env.LOGFILE}`
+    : `${startTime}-tricktionary.log`;
+  const foldername = process.env.LOGFOLDER ? process.env.LOGFOLDER : ".";
+  const paths: string[] = [foldername, filename];
+  const result = path.resolve(path.join(...paths));
+  return result;
+}
 
-const paths:string[] = [process.env.LOGFOLDER || ".", process.env.LOGFILE || "tricktionary.log"];
-const LOGFILE = path.resolve(path.join(...paths));
-console.log('logging to ', LOGFILE)
+if (process.env.WRITELOG) {
+  console.log("logging to ", getLogFilePaths());
+}
 
 /**
- * write to log file.
- * 
+ * write to log file when
+ * WRITELOG=1
+ *
  * @param entry message
  */
-export const log = function(entry:string) {
-  fs.appendFileSync(LOGFILE, `${new Date().toISOString()} - ${entry}\n`);
+export const log = function (entry: string) {
+  const message = `${new Date().toISOString()} - ${entry}\n`;
+  if (process.env.WRITELOG === "1") {
+    try {
+      fs.appendFileSync(getLogFilePaths(), message);
+    } catch (err) {
+      console.log(message);
+    }
+  } else {
+    console.log(message);
+  }
 };
