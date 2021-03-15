6b31b0be0ff12e2293993098b03fe5fd5fa46fb4
trevorjmartin <trevor.j.martin@gmail.com>
2021-03-14 20:31:34 -0400

    redisCache

--- src/api/reactions/routes.ts
+++ src/api/reactions/routes.ts
@@ -1,16 +1,55 @@
 import { Router } from "express";
-import Reactions from "./model";
+import { redisCache, TricktionaryCache } from "../middleware";
+import { getDatabaseReactions } from "./util";
+
+import { log } from "../../logger";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  // return a list of available reactions.
-  let available;
-  try {
-    available = await Reactions.getAll();
-    res.status(200).json({ available });
-  } catch (err) {
-    res.status(401).json({ error: err });
+/**
+ * request all from the database table "Reactions"
+ * @param req Request object
+ * @param res Resonse object
+ * @returns
+ */
+async function sendFromDatabase(req: any, res: any) {
+  const reply = await getDatabaseReactions(req.redis);
+  return res.json(reply);
+}
+
+router.get("/", redisCache, async (req: any, res: any, next: any) => {
+  const cache: TricktionaryCache = req.redis;
+  // no redis ?
+  if (!cache?.getValue) {
+    // not connected to redis
+    try {
+      await sendFromDatabase(req, res);
+    } catch (err) {
+      return res
+        .status(400)
+        .json({ error: err.message || "database lookup error" });
+    }
+  } else {
+    // with redis
+    try {
+      await cache.getValue(
+        "tricktionary-reactions",
+        async function (err: any, value: any) {
+          if (value && !err) {
+            return res.json(JSON.parse(value));
+          } else {
+            log(
+              "tricktionary-reactions were not found in the cache => sendFromDatabase"
+            );
+            // database
+            await sendFromDatabase(req, res);
+          }
+        }
+      );
+    } catch (err) {
+      log(err?.message || err);
+      next();
+    }
   }
 });
 
---
+++ src/api/reactions/util.ts
@@ -0,0 +1,29 @@
+import Reactions from "./model";
+import { log } from "../../logger";
+import { TricktionaryCache } from "../middleware";
+/**
+ * seconds to live in redis cache
+ */
+const CACHE_LIFETIME_REACTIONS: number = process.env.CACHE_LIFETIME_REACTIONS
+  ? Number(process.env.CACHE_LIFETIME_REACTIONS)
+  : 120;
+
+/**
+ * DB table of Reactions
+ * @returns Promise
+ */
+export async function getDatabaseReactions(cache: TricktionaryCache) {
+  try {
+    // lookup in rdb
+    const available = await Reactions.getAll();
+    // update cache when available
+    if (cache?.setValue) {
+      const stringData = JSON.stringify({ available });
+      cache.setValue("tricktionary-reactions", stringData, CACHE_LIFETIME_REACTIONS);
+    }
+    return { available };
+  } catch (err) {
+    log("error whilst getting/setting tricktionary-reactions");
+    return { error: err };
+  }
+}
