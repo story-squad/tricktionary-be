9c840d8069bc33a38f353e35103240c17c2a661d
Trevor Martin <trevor.j.martin@gmail.com>
2021-03-20 20:33:35 -0400

    added try catch blocks to await Promises

--- src/sockets/handleStartGame.ts
+++ src/sockets/handleStartGame.ts
@@ -16,32 +16,45 @@ async function handleStartGame(
   settings: any,
   hostChoice: any
 ) {
+  const defaultError = "there was an error with starting your game";
   let r = checkSettings(settings);
   if (!r.ok) {
-    handleErrorMessage(
-      io,
-      socket,
-      2006,
-      "there was an error with starting your game"
-    );
+    handleErrorMessage(io, socket, 2006, defaultError);
     return;
   }
   let { word, source } = r.settings;
   if (word.id === 0) {
-    word = await contributeWord(word.word, word.definition, source);
+    try {
+      word = await contributeWord(word.word, word.definition, source);
+    } catch (err) {
+      log(
+        `[!ERROR] handleStartGame -> contributeWord(${word.word}, ${word.definition}, ${source})`
+      );
+      handleErrorMessage(io, socket, 2006, defaultError);
+      return;
+    }
   } else {
     let r = await wordFromID(word.id);
     if (r.ok) {
       word = r.word;
     }
   }
-  let newRound = await startNewRound(
-    socket.id,
-    word,
-    lobbies,
-    lobbyCode,
-    r.settings
-  );
+  let newRound: any;
+  try {
+    newRound = await startNewRound(
+      socket.id,
+      word,
+      lobbies,
+      lobbyCode,
+      r.settings
+    );
+  } catch (err) {
+    log(
+      `[!ERROR] handleStartGame -> startNewRound by ${socket.id} with "${word.word}" from ${source}`
+    );
+    handleErrorMessage(io, socket, 2006, defaultError);
+    return;
+  }
   if (newRound.ok && newRound.result?.status === 201) {
     lobbies = newRound.lobbies;
     // update the host token
@@ -53,12 +66,14 @@ async function handleStartGame(
     } catch (err) {
       log("error recording the host's choices");
       log(err.message);
+      handleErrorMessage(io, socket, 2006, defaultError);
+      return;
     }
-
     // pub-sub update
     io.to(lobbyCode).emit("game update", lobbies[lobbyCode]);
   } else {
     log("there was a server error while starting the game");
+    handleErrorMessage(io, socket, 2006, defaultError);
   }
 }
 
