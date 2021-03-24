26097f221ff444a7a990232fb1d5a9fcae29b88c
Trevor Martin <trevor.j.martin@gmail.com>
2021-03-24 03:12:32 -0400

    add boolean status indicator

--- src/api/player/routes.ts
+++ src/api/player/routes.ts
@@ -21,9 +21,9 @@ router.get("/last-user-id/:id", async (req, res) => {
   try {
     player = await Player.findPlayer("last_user_id", user_id);
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(400).json({ ok: false, error: err.message });
   }
-  res.status(200).json({ player });
+  res.status(200).json({ ok: true, player });
 });
 
 router.put("/id/:id", async (req, res) => {
@@ -36,5 +36,5 @@ router.put("/id/:id", async (req, res) => {
     res.status(400).json({ error: err.message });
   }
   res.status(200).json({ player });
-})
+});
 export default router;
