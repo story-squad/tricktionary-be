49f1d1531b0eedf5244a71dfcf0aa72750741985
Trevor Martin <trevor.j.martin@gmail.com>
2021-03-29 21:18:41 -0400

    added GET routes to Game table

--- src/api/game/routes.ts
+++ src/api/game/routes.ts
@@ -16,4 +16,26 @@ router.post("/new", async (req, res) => {
   res.status(200).json(game_id);
 });
 
+router.get("/all", async (req, res) => {
+  let result;
+  try {
+    result = await Game.get();
+  } catch (err) {
+    result = { error: err.message };
+  }
+  return res.status(200).json(result.games);
+});
+
+router.get("/latest/:limit", async (req, res) => {
+  let limit:string = req.params.limit;
+  const n = Number(limit) > 0 ? Number(limit) : 5;
+  let result: any;
+  try {
+    result = await Game.latest(n);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+  return res.status(200).json(result);
+});
+
 export default router;
