9d9a9ec16f1fbac6eead2d6ab79da3cd7342d2e8
rmjuarez12 <rmjuarez12@gmail.com>
2022-03-28 13:09:41 -0400

    Adjusted knex production settings

--- knexfile.js
+++ knexfile.js
@@ -16,6 +16,9 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
     migrations: { directory: "./data/migrations" },
     seeds: { directory: "./data/seeds" },
   },
