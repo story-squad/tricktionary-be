89441be45f23e4c4d3e727ac580f72aa6ed61ae5
Richard Rodriguez <rmjuarez12@gmail.com>
2022-04-04 11:31:14 -0400

    Added test endpoint to check DB conn and ENV vars

--- src/api/bots/routes.ts
+++ src/api/bots/routes.ts
@@ -62,4 +62,32 @@ router.get("/botpid/:username/:lobbycode", (req, res) => {
     });
 });
 
+// WIL REMOVE SOON! I AM JUST TESTING ENV OUTPUT
+router.get("/testENV", (req, res) => {
+  const PORT = process.env.PORT;
+  const NODE_ENV = process.env.DB_ENVIRONMENT;
+  const DATABASE_URL = process.env.DATABASE_URL;
+
+  Bot.testDBConnection()
+    .then((response) => {
+      res.status(200).json({
+        port: PORT,
+        nodeENV: NODE_ENV,
+        dbURL: DATABASE_URL,
+        response: response,
+      });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          port: PORT,
+          nodeENV: NODE_ENV,
+          dbURL: DATABASE_URL,
+          message: "Error getting test connection",
+          err: err.message,
+        });
+    });
+});
+
 export default router;
