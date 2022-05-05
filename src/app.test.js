05fb90df1a991436096c186d35099de169d87c23
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-05-05 18:19:14 -0400

    cleanup and addition of returning player test

--- src/app.test.js
+++ src/app.test.js
@@ -5,10 +5,10 @@ const { ioServer } = require("./app");
 const Client = require("socket.io-client");
 const { newToken } = require("./api/auth/utils");
 
-let test_player_token;
+let default_case = { player_token: "", player_token_old: "" };
 
 jest.setTimeout(1 * 5 * 1000);
-describe("my awesome project", () => {
+describe("Socket.io tests", () => {
   let serverSocket, clientSocket;
 
   beforeAll((done) => {
@@ -33,6 +33,14 @@ describe("my awesome project", () => {
     clientSocket.close();
   });
 
+  test("communicates", (done) => {
+    clientSocket.on("hello", (arg) => {
+      expect(arg).toBe("world");
+      done();
+    });
+    serverSocket.emit("hello", "world");
+  });
+
   test("test new player login", (done) => {
     clientSocket.emit("login", "", async (arg1) => {
       expect(arg1.ok).toBe(true);
@@ -41,16 +49,22 @@ describe("my awesome project", () => {
         .from("Player")
         .where("token", arg1.token);
       expect(temp[0].last_user_id).toBe(clientSocket.id);
-      test_player_token = arg1.token;
-    });
-    done();
-  });
-
-  test("communicates", (done) => {
-    clientSocket.on("hello", (arg) => {
-      expect(arg).toBe("world");
+      default_case.player_token = arg1.token;
       done();
     });
-    serverSocket.emit("hello", "world");
+  });
+
+  test("test returning player login", (done) => {
+    clientSocket.on("token update", () => {
+      console.log("new token\n" + default_case.player_token);
+      console.log("old token\n" + default_case.player_token_old);
+      done();
+    });
+    default_case.player_token_old = default_case.player_token;
+    clientSocket.emit("login", default_case.player_token, async (arg1) => {
+      console.log(arg1);
+      expect(arg1.ok).toBe(true);
+      default_case.player_token = arg1.token;
+    });
   });
 });
